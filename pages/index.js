7719f6d98db1e565d55cd32f3f86c287dc9013b5
chaipa <chaitanya.patel@flemingcollege.ca>
2020-07-13 15:57:33 -0400

    fetch data and display data on UI

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,57 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
 
-export default function Home() {
+
+
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+
+export default function Home({ data }) {
+  console.log('data', data)
+
+  const {results=[]}=data;
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Characters <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Rick and Morty Character 
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className="grid">
+          {results.map(result=>{
+            const { id, name,image }= result;
+          return(
+            <li className="card" key={id}>
+            <a href="#">
+          
+            <img src={image} alt={`${name} Thumbnail`} />
+          <h3>{name}</h3>
+            
+           
+            </a>
+          </li>
+          )  
+          })}
+         
+        </ul>
       </main>
 
       <footer>
@@ -143,9 +149,11 @@ export default function Home() {
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
           margin-top: 3rem;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
         }
 
         .card {
